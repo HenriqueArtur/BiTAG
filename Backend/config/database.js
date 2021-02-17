cb83579b823ca20cf613af43c1e90e0d3dcfd11e
Henrique Artur Cordeiro Gomes <henriqueacg@gmail.com>
2021-02-17 18:48:34 -0300

    add banco de dados

---
+++ Backend/config/database.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: "root",
+    password: "admin123",
+    database: "projeto_2_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: "admin123",
+    database: "projeto_2_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: "admin123",
+    database: "projeto_2_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
---
+++ Backend/database/migrations/20210217195223-create-games.js
@@ -0,0 +1,84 @@
+module.exports = {
+  up: (queryInterface, DataTypes) => {
+    return queryInterface.createTable("Games", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        unique: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      price: {
+        allowNull: true,
+        type: DataTypes.FLOAT,
+      },
+      inital_price: {
+        allowNull: true,
+        type: DataTypes.FLOAT,
+      },
+      revenue: {
+        allowNull: false,
+        type: DataTypes.FLOAT,
+      },
+      positive_reviews: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      negative_reviews: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      owners: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      release_date: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      website: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      developer_name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      publisher_name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      header_image: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      about: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      short_description: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      detailed_description: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable("Games");
+  },
+};
