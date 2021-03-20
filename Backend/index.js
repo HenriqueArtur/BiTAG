782b2de5ba09ba9bddde7bd6fd98082031f3fcdb
Henrique Artur Cordeiro Gomes <henriqueacg@gmail.com>
2021-03-19 23:39:04 -0300

    :sparkles: endpoints game
    new endpoints game controller

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,14 @@
 import "dotenv/config";
 import cors from "cors";
 const express = require("express");
-const route = require("./app/routes/route.js");
 const bodyParser = require("body-parser");
 
 const Tag = require("./app/controllers/tag.controller");
+const Game = require("./app/controllers/game.controller");
 
 const app = express();
 
 app.use(cors());
-app.use("./api", route);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -20,6 +19,12 @@ app.get("/", (req, res) => {
 app.get("/tags", Tag.findAll);
 app.get("/tags/findByName", Tag.findByName);
 app.get("/tags/findByOptions", Tag.findByOptions);
+
+app.get("/api/games", Game.findAll);
+app.get("/api/games/findByName", Game.findByName);
+app.get("/api/games/findByAppId", Game.findByAppId);
+app.get("/api/games/findByTags", Game.findByTags);
+app.get("/api/games/findByParams", Game.findByParams);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on port: ${process.env.PORT}`);
